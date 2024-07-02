9cbcb518525e17b5a0c7a74997a90c7c025394f7
Gokruzk <nigell@gmail.com>
2024-07-01 20:08:16 -0500

    feat: :art: format code

--- api/userAPI.tsx
+++ api/userAPI.tsx
@@ -122,7 +122,6 @@ export async function decrypt(input: string): Promise<any> {
   const { payload } = await jwtVerify(input, key, {
     algorithms: ["HS256"],
   });
-  console.log(payload);
   return payload;
 }
 
