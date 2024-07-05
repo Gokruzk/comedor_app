e51b3426cfc118e8d90cfb5320fdf3ce6524117b
Gokruzk <nigell@gmail.com>
2024-07-04 20:11:08 -0500

    api_url

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 const nextConfig = {
     experimental: {
         serverActions: {
-            allowedOrigins: ["localhost:8000", "localhost:8888", "localhost:3000", "https://comedor-api.onrender.com/","*"]
+            allowedOrigins: ["localhost:8000", "localhost:8888", "localhost:3000", API_URL,"*"]
         }
     }
 };
