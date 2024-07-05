3d2e1292c34491c94a3ec5c2604db2c5f0426478
Gokruzk <nigell@gmail.com>
2024-07-04 20:09:00 -0500

    origins

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     experimental: {
         serverActions: {
-            allowedOrigins: ["localhost:8000", "localhost:8888", "localhost:3000", "*"]
+            allowedOrigins: ["localhost:8000", "localhost:8888", "localhost:3000", "https://comedor-api.onrender.com/","*"]
         }
     }
 };
