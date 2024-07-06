414447a3e17d340cc05289988ab9277e6f3df807
Gokruzk <nigell@gmail.com>
2024-07-05 21:59:58 -0500

    style: :recycle: button title

--- app/admin_agregarmenu/page.tsx
+++ app/admin_agregarmenu/page.tsx
@@ -215,7 +215,7 @@ const AgregarMenu = () => {
                 type="submit"
                 className="bg-blue-500 hover:bg-blue-700 w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
-                Registrar
+                Agregar menú
               </button>
             </form>
           </div>
