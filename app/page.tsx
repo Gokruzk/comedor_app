5dabdc9ec77b2b5403381ac5cee5f75634888658
Sarkosky <sivelez96@gmail.com>
2024-07-08 17:36:28 -0500

    13221

--- app/page.tsx
+++ app/page.tsx
@@ -35,9 +35,6 @@ export default function Home() {
             <LinkButton href="/login" style="focus:outline-none text-white bg-green-700 hover:bg-green-200 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-xl px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" title="INICIAR ->"/>
           </div>
         </div>
-
-
-
       </div>
     </main>
   );
