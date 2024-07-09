11442f47d9643433df0a8435462c794ededafa06
Sarkosky <sivelez96@gmail.com>
2024-07-08 23:28:04 -0500

    interfaz_perfil

--- app/perfil/layout.tsx
+++ app/perfil/layout.tsx
@@ -9,6 +9,7 @@ import { APP_NAME } from "@/constants";
 import userStore from "@/store/auth/userStore";
 import { logout } from "@/api/userAPI";
 import { getUserSession } from "@/utils";
+import { NavBarProps, LinkButtonProps } from "@/types";
 
 const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
@@ -29,7 +30,7 @@ const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
         authUser(user);
         router.push("/perfil");
       }
-      setIsSuccess(true);
+      setIsSuccess(true); 
     })();
   }, [router, authUser]);
 
@@ -58,6 +59,7 @@ const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
       <header>
+        {/*
         <nav className="bg-white border-gray-200 dark:bg-gray-500 dark:border-gray-700">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a
@@ -118,13 +120,11 @@ const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
               </ul>
             </div>
           </div>
-        </nav>
-      </header>
+        </nav>*/}
+
+{/*SCRIPT PARA LA BARRA DE NAVEGACION */}
 
 
-      {/*===============================================================================================================================================================*/}
-      {/*=========================================================codigo para la barra de navegacion====================================================================*/}
-      {/*===============================================================================================================================================================*/}
           <nav className="bg-white border-gray-200 dark:bg-white dark:border-white my-4">
             <div className="max-w-screen-xl mx-auto">
               <div className="max-w-6xl w-full mx-auto bg-red-600 rounded-2xl p-4">
@@ -136,85 +136,61 @@ const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
 
                   {/*script para el boton en pantallas pequenias*/}
                   <button
-                      onClick={toggleMenu} // Toggle menu visibility
-                      type="button"
-                      className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-700 dark:text-white dark:hover:bg-red-700 dark:focus:ring-red-600"
-                      aria-controls="navbar-dropdown"
-                      aria-expanded={menuOpen ? "true" : "false"} // Toggle aria-expanded attribute
+                    onClick={toggleMenu} // Toggle menu visibility
+                    type="button"
+                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    aria-controls="navbar-dropdown"
+                    aria-expanded={menuOpen ? "true" : "false"} // Toggle aria-expanded attribute
+                  >
+                    <span className="sr-only">Open main menu</span>
+                    <svg
+                      className="w-5 h-5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 17 14"
                     >
-                      <span className="sr-only">Open main menu</span>
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M1 1h15M1 7h15M1 13h15"
+                      />
+                    </svg>
+                  </button>
+                  <div
+                    className={`md:flex ${
+                      menuOpen ? "block" : "hidden"
+                    } w-full md:w-auto`}
+                    id="navbar-dropdown"
+                  >
+                      <ul className="flex flex-col font-medium p-4 px-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-red-900 md:dark:bg-red-600 dark:border-red-700">
                       
-                      <svg
-                        className="w-5 h-5"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 17 14"
-                      >
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M1 1h15M1 7h15M1 13h15"
-                        />
-                      </svg>
-                    </button>
+                        <li>
+                          <LinkButton
+                            href="/nosotros"
+                            style="w-40 align-center bg-red-900 block py-2 text-white rounded bg-red-600 hover:bg-red-700 md:border-0 dark:text-white md:bg-red-600"
+                            title="Acerca de"
+                          />
+                        </li>
 
-
-                  <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-                    <ul className="flex flex-col font-medium py-0 px-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-red-600 md:dark:bg-red-600 dark:border-red-700">
-
-                      <li>
-                          <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 px-4 text-gray-900 rounded hover:bg-red-700 md:hover:bg-red-700 md:border-0  md:w-auto dark:text-white  dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-red-700">Menú</button>
-
-                          {/*dropdown*/}
-                          <div id="dropdownNavbar" className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-red-700 dark:divide-gray-600">
-                              <ul className="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
-                                <li>
-                                  <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                                </li>
-                                <li>
-                                  <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                                </li>
-                                <li>
-                                  <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                                </li>
-                              </ul>
-                              <div className="py-1">
-                                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-                              </div>
-                          </div>
-                      </li>
-
-                      <li>
-                        <a href="#" className="align-center block py-2 px-4 text-white rounded bg-red-600 hover:bg-red-700 md:border-0 dark:text-white">Acerca de</a>
-                      </li>
-
-                      <li>
-                        <LogoutButton
-                          style="align-center block py-2 px-4 text-white rounded bg-red-600 hover:bg-red-700 md:border-0 dark:text-white"
-                          title="Mi cuenta"
-                          onClick={handleLogout}
-                        />
-                      </li>
-
-                      <li>
-                        <LogoutButton
-                          style="align-center block py-2 px-4 text-white rounded bg-red-600 hover:bg-red-700 md:border-0 dark:text-white"
-                          title="Cerrar sesión"
-                          onClick={handleLogout}
-                        />
-                      </li>
-                    </ul>
+                        <li>
+                          <LogoutButton
+                            style="w-40 align-center bg-red-900 block py-2 text-white rounded bg-red-600 hover:bg-red-700 md:border-0 dark:text-white md:bg-red-600"
+                            title="Cerrar sesión"
+                            onClick={handleLogout}
+                          />
+                        </li>
+                      </ul>
+                    
                   </div>
                 </div>
               </div>
             </div>
           </nav>
 
-
-
+      </header>
 
       {children}
     </main>
