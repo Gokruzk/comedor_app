75e0fb34d4c73fd1cce3dd6dad003e900b038eec
Gokruzk <nigell@gmail.com>
2024-07-09 07:15:51 -0500

    agregar saldo

--- app/cuenta_info/page.tsx
+++ app/cuenta_info/page.tsx
@@ -51,7 +51,6 @@ function CuentaInfo() {
 
   const linkbuttons = [
     { href: "/agregar_tarjeta", title: "Agregar tarjeta" },
-    { href: "/agregar_saldo", title: "Agregar saldo" },
   ];
 
   let cardData: any = {};
@@ -62,7 +61,7 @@ function CuentaInfo() {
         <NavBar
           title="<- Volver"
           href="/cuenta"
-          nbuttons={2}
+          nbuttons={1}
           linkbuttons={linkbuttons}
         />
         <div className="flex-1 p-6">
@@ -113,6 +112,60 @@ function CuentaInfo() {
               {/* {cardData.saldo} */}
               $0
             </p>
+          </div>
+        </div>
+        <div className="mx-auto max-w-5xl">
+          <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
+            Agrega saldo a tu cuenta
+          </h2>
+          <div className="mt-6 sm:mt-8 lg:flex lg:items-start lg:gap-12">
+            <div className="mt-6 grow sm:mt-8 lg:mt-0">
+              <div className="space-y-4 rounded-lg border border-gray-100 bg-gray-50 p-6 dark:border-gray-700 dark:bg-gray-800">
+                <div className="space-y-2">
+                  <dl className="flex items-center justify-between gap-4">
+                    <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                      Original price
+                    </dt>
+                    <dd className="text-base font-medium text-gray-900 dark:text-white">
+                      $6,592.00
+                    </dd>
+                  </dl>
+
+                  <dl className="flex items-center justify-between gap-4">
+                    <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                      Savings
+                    </dt>
+                    <dd className="text-base font-medium text-green-500">
+                      -$299.00
+                    </dd>
+                  </dl>
+                  <dl className="flex items-center justify-between gap-4">
+                    <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                      Store Pickup
+                    </dt>
+                    <dd className="text-base font-medium text-gray-900 dark:text-white">
+                      $99
+                    </dd>
+                  </dl>
+                  <dl className="flex items-center justify-between gap-4">
+                    <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                      Tax
+                    </dt>
+                    <dd className="text-base font-medium text-gray-900 dark:text-white">
+                      $799
+                    </dd>
+                  </dl>
+                </div>
+                <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 dark:border-gray-700">
+                  <dt className="text-base font-bold text-gray-900 dark:text-white">
+                    Total
+                  </dt>
+                  <dd className="text-base font-bold text-gray-900 dark:text-white">
+                    $7,191.00
+                  </dd>
+                </dl>
+              </div>
+            </div>
           </div>
         </div>
       </div>
