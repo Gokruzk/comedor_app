cbc9fdf3a8495dce2569a48b30357d14282fc1d3
Gokruzk <nigell@gmail.com>
2024-07-09 09:44:34 -0500

    loading purchase

--- app/compra/[id_menu]/page.tsx
+++ app/compra/[id_menu]/page.tsx
@@ -30,8 +30,10 @@ export default function CompraQ({ params }: Params_Menu) {
 const CompraMenu = ({ id_menu }: MenuForm) => {
   const router = useRouter();
   const [selectedTime, setSelectedTime] = useState(0);
+  const [loadingPurchase, setIsLoading] = useState<boolean>(false);
 
   const buyMenu = async (formdata: any) => {
+    setIsLoading(true);
     const { user } = await getUserSession();
     const email = user;
 
@@ -83,7 +85,7 @@ const CompraMenu = ({ id_menu }: MenuForm) => {
       toast.success(mensaje);
       setTimeout(() => {
         router.push("/comidas");
-      }, 2000);
+      }, 1000);
     } else {
       toast.error(mensaje);
     }
@@ -100,27 +102,30 @@ const CompraMenu = ({ id_menu }: MenuForm) => {
       } else {
         showToastMessage(`${data.error}`, "error");
       }
+      setIsLoading(false);
     },
     onError: (error) => {
       console.log(error);
       showToastMessage(`${error}`, "error");
+      setIsLoading(false);
     },
   });
 
-if (isLoading) {
-  return (
-    <main>
-      <Loading />
-    </main>
-  );
-} else if (isError)
-  return (
-    <main className="bg-gray-50 dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        Error {error?.message}
-      </div>
-    </main>
-  );
+  if (isLoading || loadingPurchase) {
+    return (
+      <main>
+        <Loading />
+        <ToastContainer />
+      </main>
+    );
+  } else if (isError)
+    return (
+      <main className="bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          Error {error?.message}
+        </div>
+      </main>
+    );
 
   return (
     <main className="bg-gray-50 dark:bg-gray-900">
