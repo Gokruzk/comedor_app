f351f2972bd8904020f38eb1680331c063464a62
Sarkosky <sivelez96@gmail.com>
2024-07-09 12:54:20 -0500

    qwerdfgdsd

--- app/admin_agregarmenu/page.tsx
+++ app/admin_agregarmenu/page.tsx
@@ -75,18 +75,12 @@ const AgregarMenu = () => {
   });
 
   return (
-    <main className="bg-gray-50 dark:bg-gray-900">
+    <main className="bg-gray-50 dark:bg-gray-100">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-white rounded-lg shadow-2xl dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-white dark:border-white">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-              <LinkButton
-                title="<- Volver"
-                href="/admin_comidas"
-                style="font-medium text-primary-600 hover:underline dark:text-primary-500"
-              />
-            </p>
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-gray-600">
               Agregar menu de comida
             </h1>
             <form
@@ -96,7 +90,7 @@ const AgregarMenu = () => {
               <div>
                 <label
                   htmlFor="menu_title"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-600"
                 >
                   Título del menú
                 </label>
@@ -109,7 +103,7 @@ const AgregarMenu = () => {
                 />
                 <label
                   htmlFor="menu_description"
-                  className="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-gray-600"
                 >
                   Descripción del menú
                 </label>
@@ -123,7 +117,7 @@ const AgregarMenu = () => {
 
                 <label
                   htmlFor="price"
-                  className="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-gray-600"
                 >
                   Precio
                 </label>
@@ -140,13 +134,13 @@ const AgregarMenu = () => {
                 />
                 <label
                   htmlFor="id_menu_type"
-                  className="block mt-4 mb-2 text-sm font-bold text-gray-900 dark:text-white"
+                  className="block mt-4 mb-2 text-sm font-bold text-gray-900 dark:text-gray-600"
                 >
                   Tipo de menú
                 </label>
                 <select
                   id="id_menu_type"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-red-600 dark:focus:border-red-600"
                   {...register("id_menu_type")}
                 >
                   <option defaultValue={"Tipo de usuario"}>Tipo de menú</option>
@@ -160,7 +154,7 @@ const AgregarMenu = () => {
                   datepicker-autoselect-today="true"
                   className="mx-auto sm:mx-0 flex justify-center [&>div>div]:shadow-none [&>div>div]:bg-gray-50 [&_div>button]:bg-gray-50"
                 ></div>
-                <label className="mt-4 text-sm font-bold text-gray-900 dark:text-white mb-2 block">
+                <label className="mt-4 text-sm font-bold text-gray-900 dark:text-gray-600 mb-2 block">
                   Seleccionar el horario en el que menú estará disponible
                 </label>
                 <ul
@@ -214,12 +208,24 @@ const AgregarMenu = () => {
                   </li>
                 </ul>
               </div>
-              <button
-                type="submit"
-                className="bg-blue-500 hover:bg-blue-700 w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Agregar menú
-              </button>
+
+              <div className="flex justify-center items-center">
+                <button
+                  type="submit"
+                  className="bg-green-600 hover:bg-green-800 w-2/3 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Agregar menú
+                </button>
+              </div>
+
+              <div className="flex justify-center items-center">
+                <LinkButton
+                  href={"submit"}
+                  style="bg-blue-500 hover:bg-blue-700 w-2xl text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800" title="Cancelar y volver"
+                />
+              </div>
+              
+
             </form>
           </div>
         </div>
