477639faaa2feaf561bacb49e0d9c92f132d5f3e
Sarkosky <sivelez96@gmail.com>
2024-07-09 12:26:49 -0500

    actualizacion xyz

--- app/admin_comidas/page.tsx
+++ app/admin_comidas/page.tsx
@@ -162,7 +162,7 @@ function Comidas() {
             </div>
           </div>
           <div>
-            <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">
+            <h2 className="text-2xl font-normal text-gray-900 dark:text-gray-600 mb-4">
               Menús no disponibles
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
