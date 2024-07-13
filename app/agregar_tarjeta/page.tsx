9f1ba0b9e067da193972d8cbb64138f7ffb46744
Sarkosky <sivelez96@gmail.com>
2024-07-13 12:55:08 -0500

    asas

--- app/agregar_tarjeta/page.tsx
+++ app/agregar_tarjeta/page.tsx
@@ -182,7 +182,7 @@ const AgregarTarjetaPage = () => {
                       role="tooltip"
                       className="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
                     >
-                      The last 3 digits on back of card
+                      Los últimos 3 dígitos de la parte trase de su tarjeta
                       <div className="tooltip-arrow" data-popper-arrow></div>
                     </div>
                   </label>
