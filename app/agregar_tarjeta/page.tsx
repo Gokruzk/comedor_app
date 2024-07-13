b2313152c2b5e0a696a463b21d8bc147671686fd
Sarkosky <sivelez96@gmail.com>
2024-07-12 22:37:46 -0500

    card

--- app/agregar_tarjeta/page.tsx
+++ app/agregar_tarjeta/page.tsx
@@ -142,27 +142,24 @@ const AgregarTarjetaPage = () => {
                       <svg
                         className="h-4 w-4 text-gray-500 dark:text-gray-400"
                         aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
+                
                         width="24"
                         height="24"
                         fill="currentColor"
                         viewBox="0 0 24 24"
                       >
-                        <path
-                          fillRule="evenodd"
-                          d="M5 5a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1 2 2 0 0 1 2 2v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a2 2 0 0 1 2-2ZM3 19v-7a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Zm6.01-6a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm-10 4a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z"
-                          clipRule="evenodd"
-                        />
+                        
                       </svg>
                     </div>
                     <input
-                      datepicker-format="mm/yy"
                       id="card_expiration"
                       type="text"
                       {...register("card_expiration")}
                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-100 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-800 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="mm/yy"
+                      placeholder="MM/YY"
                       required
+                      pattern="\d{2}/\d{2}"
+                      maxLength={5}
                     />
                   </div>
                 </div>
